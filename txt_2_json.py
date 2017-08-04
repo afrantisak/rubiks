008d6908969fdb40eaf6a05cb29b691ad4f95446
Aaron Frantisak <afrantisak@gmail.com>
2017-08-04 17:43:01 -0500

    convert to json

--- txt_2_json.py
+++ txt_2_json.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python2
+import json
+import collections
 
 for line in open('corners.txt'):
     tokens = line.split(' ', 4)
     name, face1, face2, _dash, moves = tokens
     name = name[:-1]
     move = moves.split()
-    print name, face1, face2,  move
+    json_obj = collections.OrderedDict([
+        ('name', name),
+        ('face1', face1),
+        ('face2', face2),
+        ('move', move)
+    ])
+    print json.dumps(json_obj)
