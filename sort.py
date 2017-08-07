648f227877e0ccb272971b7846125b1d5b5ab642
Aaron Frantisak <afrantisak@gmail.com>
2017-08-07 17:15:41 -0500

    images

--- sort.py
+++ sort.py
@@ -9,4 +9,11 @@ for json_str in open('corners.json'):
     moves.append(json_obj)
 
 for move in sorted(moves, key=lambda move: move['move']):
-    print json.dumps(move)
+    name = move['name']
+    move_str = ' '.join(move['move'])
+    img = '<img src="file:///home/franta/dev/rubiks/{name}.png"/>'.format(**locals())
+    print '<html>'
+    print '<body>'
+    print img, json.dumps(move_str), '<BR>'
+    print '</body>'
+    print '</html>'
